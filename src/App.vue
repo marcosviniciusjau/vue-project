ee96f0ace2614b946f557a8a67f403f7a85f9f9d
marcosviniciusjau <marcosviniciusjahu@gmail.com>
2023-08-08 15:50:03 -0300

    vue key

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,42 @@
 <template>
   <h1>Food</h1>
-  <p>The user can now change the favorite status of the food items with the use of a new data property initialized by the 'is-favorite' prop value.</p>
+  <p>Food items are generated with v-for from the 'foods' array.</p>
   <div id="wrapper">
-    <food-item 
-      food-name="Apples" 
-      food-desc="Apples are a type of fruit that grow on trees."
-      v-bind:is-favorite="true"/>
-    <food-item 
-      food-name="Pizza"
-      food-desc="Pizza has a bread base with tomato sauce, cheese, and toppings on top."
-      v-bind:is-favorite="false"/>
-    <food-item 
-      food-name="Rice"
-      food-desc="Rice is a type of grain that people like to eat."
-      v-bind:is-favorite="false"/>
-  </div> 
+    <food-item
+  v-for="x in foods"
+  :key="x.name"
+  :food-name="x.name"
+  :food-desc="x.desc"
+  :is-favorite="x.favorite"
+/>
+  </div>
 </template>
 
-<script></script>
-
+<script>
+  export default {
+    data() {
+      return {
+        foods: [
+          { name: 'Apples',
+            desc: 'Apples are a type of fruit that grow on trees.',
+            favorite: true },
+          { name: 'Pizza',
+            desc: 'Pizza has a bread base with tomato sauce, cheese, and toppings on top.',
+            favorite: false },
+          { name: 'Rice',
+            desc: 'Rice is a type of grain that people like to eat.',
+            favorite: false },
+          { name: 'Fish',
+            desc: 'Fish is an animal that lives in water.',
+            favorite: true },
+          { name: 'Cake',
+            desc: 'Cake is something sweet that tastes good.',
+            favorite: false }
+        ]
+      };
+    }
+  }
+</script>
 <style>
   #wrapper {
     display: flex;
--- src/components/FoodItem.vue
+++ src/components/FoodItem.vue
@@ -1,11 +1,33 @@
 <template>
   <div>
-    <h2>{{ foodName }}</h2>
+    <h2>
+      {{ foodName }}
+      <img src="/img_quality.svg" v-show="foodIsFavorite">
+    </h2>
+    <p>{{ foodDesc }}</p>
+    <button v-on:click="toggleFavorite">Favorite</button>
   </div>
 </template>
 
 <script>
   export default {
-    props: ['foodName']
+    props: ['foodName','foodDesc','isFavorite'],
+    data() {
+      return {
+        foodIsFavorite: this.isFavorite
+      }
+    },
+    methods: {
+      toggleFavorite() {
+        this.foodIsFavorite = !this.foodIsFavorite;
+      }
+    }
   }
 </script>
+
+<style>
+  img {
+    height: 1.5em;
+    float: right;
+  }
+</style>
