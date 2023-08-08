1c7c3c744c09fa8427a45048759c5f5fc39def32
marcosviniciusjau <marcosviniciusjahu@gmail.com>
2023-08-08 15:40:51 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>Food</h1>
+  <p>Components created with v-for based on an array.</p>
+  <div id="wrapper">
+    <food-item
+      v-for="x in foods"
+      v-bind:food-name="x"/>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        foods: ['Apples','Pizza','Rice','Fish','Cake']
+      };
+    }
+  }
+</script>
