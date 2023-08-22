2f25bc5c41f5cd4468129b24eea491ff41e94657
marcosviniciusjau <marcosviniciusjahu@gmail.com>
2023-08-22 12:06:03 -0300

    animations with v-for

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,96 @@
 <template>
-  <p>Choose what part of this page you want to see:</p>
-  <router-link to="/animals">Animals</router-link>
-  <router-link to="/food">Food</router-link><br>
-  <div>
-    <router-view></router-view>
-  </div>
+  <h3>The &lt;TransitionGroup&gt; Component</h3>
+  <p>Items inside the &lt;TransitionGroup&gt; component are animated when they are created or removed.</p>
+  <button @click="addDie">Roll</button>
+  <button @click="addDie10">Roll 10 dice</button>
+  <button @click="dice.sort(compareFunc)">Sort</button>
+  <button @click="dice.sort(shuffleFunc)">Shuffle</button><br>
+  <TransitionGroup>
+    <div 
+    v-for="x in dice" 
+    :key="x.keyNmbr" 
+    class="diceDiv" 
+    :style="{ backgroundColor: 'hsl('+x.dieNmbr*60+',85%,85%)' }"
+    @click="removeDie(x.keyNmbr)">
+      {{ x.dieNmbr }}
+    </div>
+  </TransitionGroup>
 </template>
 
 <script>
-export default {
-  data() {
+  export default {
+    data() {
       return {
-        activeComp: ''
+        dice: [],
+        keyNumber: 0
       }
+    },
+    methods: {
+      addDie() {
+        const newDie = {
+          dieNmbr: Math.ceil(Math.random()*6),
+          keyNmbr: this.keyNumber
+        };
+        this.dice.splice(Math.floor(Math.random()*this.dice.length),0,newDie);
+        this.keyNumber++;
+      },
+      addDie10() {
+        for(let i=0; i<10; i++) {
+          this.addDie();
+        }
+      },
+      compareFunc(a,b){
+        return a.dieNmbr - b.dieNmbr;
+      },
+      shuffleFunc(a,b){
+        return Math.random()-0.5;
+      },
+      removeDie(key) {
+        const pos = this.dice.map(e => e.keyNmbr).indexOf(key);
+        this.dice.splice(pos, 1);
+      }
+    },
+    mounted() {
+      this.addDie10();
     }
-}
+  }
 </script>
 
-<style scoped>
-  a {
-    display: inline-block;
-    background-color: black;
-    border: solid 1px black;
-    color: white;
-    padding: 5px;
-    margin: 10px;
-  }
-  a:hover,
-  a.router-link-active {
-    background-color: rgb(110, 79, 13);
-  }
-  div {
-    border: dashed black 1px;
-    padding: 20px;
-    margin: 10px;
-    display: inline-block;
-  }
-</style>   
+<style>
+.v-enter-from {
+  opacity: 0;
+  scale: 0;
+  rotate: 360deg;
+}
+.v-enter-to {
+  opacity: 1;
+  scale: 1;
+  rotate: 0deg;
+}
+.v-enter-active,
+.v-leave-active,
+.v-move {
+  transition: all 0.7s;
+}
+.v-leave-active { position: absolute; }
+.v-leave-from { opacity: 1; }
+.v-leave-to { opacity: 0; }
+.diceDiv {
+  margin: 10px;
+  width: 30px;
+  height: 30px;
+  line-height: 30px;
+  vertical-align: middle;
+  text-align: center;
+  border: solid black 1px;
+  border-radius: 5px;
+  display: inline-block;
+}
+.diceDiv:hover {
+  cursor: pointer;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+}
+#app {
+  position: relative;
+}
+</style> 
--- src/main.js
+++ src/main.js
@@ -1,16 +1,7 @@
 import { createApp } from 'vue'
-import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
-import FoodItem from './components/FoodItem.vue'
-import AnimalCollection from './components/AnimalCollection.vue'
 
-const router = createRouter({
-    history: createWebHistory(),
-    routes: [
-        { path: '/animals', component: AnimalCollection },
-        { path: '/food', component: FoodItem },
-    ]
-});
+
 const app = createApp(App)
-app.use(router);
+
 app.mount('#app')
